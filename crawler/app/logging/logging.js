56528411c48b465a32adf6964d2aa5ea518be943
Tim Neil <tim.neil@rbi.co.uk>
2017-03-10 13:57:10 +0000

    - Filtering by property and value combinations  -- Working filtering  -- Missing filtering for 'either' valueType

--- crawler/app/logging/logging.js
+++ crawler/app/logging/logging.js
@@ -1,14 +1,16 @@
-"use strict";
-function logInfo(logMessage) {
-    console.log(logMessage);
-}
-exports.logInfo = logInfo;
-function logVerboseInfo(logMessage) {
-    console.log(logMessage);
-}
-exports.logVerboseInfo = logVerboseInfo;
-function logError(logMessage) {
-    console.error(logMessage);
-}
-exports.logError = logError;
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    function logInfo(logMessage) {
+        console.log(logMessage);
+    }
+    exports.logInfo = logInfo;
+    function logVerboseInfo(logMessage) {
+        console.log(logMessage);
+    }
+    exports.logVerboseInfo = logVerboseInfo;
+    function logError(logMessage) {
+        console.error(logMessage);
+    }
+    exports.logError = logError;
+});
 //# sourceMappingURL=logging.js.map
---
+++ results/addPropertyAndValueFilterViewModel.js
@@ -0,0 +1,32 @@
+define(["require", "exports", "./propertyAndValueFilter"], function (require, exports, propertyAndValueFilter_1) {
+    "use strict";
+    class AddPropertyAndValueFilterViewModel {
+        constructor($addPropertyAndValueFilter) {
+            this.$addPropertyAndValueFilter = $addPropertyAndValueFilter;
+            this.propertyName = ko.observable('');
+            this.value = ko.observable('');
+            this.valueTypeString = ko.observable('');
+            this.propertyAndValueFilters = ko.observableArray();
+            this.add = () => {
+                this.propertyAndValueFilters.push(new propertyAndValueFilter_1.PropertyAndValueFilter(this.propertyName(), this.value(), this.valueType(), `Filter where ${this.propertyName()} of ${this.valueTypeString()} is ${this.value()}`));
+            };
+            this.openDialog = () => {
+                this.$addPropertyAndValueFilter.modal('show');
+            };
+            this.valueType = ko.computed(() => {
+                switch (this.valueTypeString()) {
+                    case 'original':
+                        return propertyAndValueFilter_1.valueType.original;
+                    case 'comparand':
+                        return propertyAndValueFilter_1.valueType.comparand;
+                    case 'either':
+                        return propertyAndValueFilter_1.valueType.either;
+                    default:
+                        return propertyAndValueFilter_1.valueType.original;
+                }
+            });
+        }
+    }
+    exports.AddPropertyAndValueFilterViewModel = AddPropertyAndValueFilterViewModel;
+});
+//# sourceMappingURL=addPropertyAndValueFilterViewModel.js.map
